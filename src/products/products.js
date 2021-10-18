484c71fd4f0b9db51a75cf054761f7f6d9d3659d
Michał Lisowiec <miclisowiec@gmail.com>
2021-10-18 21:40:45 +0200

    Add images and data for products

---
+++ src/products/products.js
@@ -0,0 +1,304 @@
+import pralkaSzajsungEkstraClean from '../images/pralkaSzajsungEkstraClean.png';
+import pralkaBekaMegaPro from '../images/pralkaBekaMegaPro.png';
+import odkurzaczFilipsSuperShine from '../images/odkurzaczFilipsSuperShine.png';
+import odkurzaczSzamsungHiperFlat from '../images/odkurzaczSzamsungHiperFlat.png';
+import odkurzaczLelektroluksDuoPower from '../images/odkurzaczLelektroluksDuoPower.png';
+import telewizorFilipsEasyControl from '../images/telewizorFilipsEasyControl.png';
+import telewizorKsajomiGigaScreen from '../images/telewizorKsajomiGigaScreen.png';
+import telewizorFilipsUltraLight from '../images/telewizorFilipsUltraLight.png';
+import głośnikJotbelPerfectSound from '../images/głośnikJotbelPerfectSound.png';
+import głośnikMarszalHighVolume from '../images/głośnikMarszalHighVolume.png';
+import lodówkaElgSideBySide from '../images/lodówkaElgSideBySide.png';
+import lodówkaAmikaLightWhite from '../images/lodówkaAmikaLightWhite.png';
+import lodówkaBekaFreshHarvest from '../images/lodówkaBekaFreshHarvest.png';
+import lodówkaSzamsungMysteriousBlack from '../images/lodówkaSzamsungMysteriousBlack.png';
+import lodówkaAmikaFlashInside from '../images/lodówkaAmikaFlashInside.png';
+import dronGreenUfo from '../images/dronGreenUfo.png';
+import dronCrazyWasp from '../images/dronCrazyWasp.png';
+import kuchniaLelektroluksHotAir from '../images/kuchniaLelektroluksHotAir.png';
+import kuchniaLelektroluksEasyGrill from '../images/kuchniaLelektroluksEasyGrill.png';
+import kuchniaAmikaFastCook from '../images/kuchniaAmikaFastCook.png';
+import pralkaSzajsungEkstraClean2 from '../images/pralkaSzajsungEkstraClean2.png';
+import pralkaBekaMegaPro2 from '../images/pralkaBekaMegaPro2.png';
+import odkurzaczFilipsSuperShine2 from '../images/odkurzaczFilipsSuperShine2.png';
+import odkurzaczSzamsungHiperFlat2 from '../images/odkurzaczSzamsungHiperFlat2.png';
+import odkurzaczLelektroluksDuoPower2 from '../images/odkurzaczLelektroluksDuoPower2.png';
+import telewizorFilipsEasyControl2 from '../images/telewizorFilipsEasyControl2.png';
+import telewizorKsajomiGigaScreen2 from '../images/telewizorKsajomiGigaScreen2.png';
+import telewizorFilipsUltraLight2 from '../images/telewizorFilipsUltraLight2.png';
+import głośnikJotbelPerfectSound2 from '../images/głośnikJotbelPerfectSound2.png';
+import głośnikMarszalHighVolume2 from '../images/głośnikMarszalHighVolume2.png';
+import lodówkaElgSideBySide2 from '../images/lodówkaElgSideBySide2.png';
+import lodówkaAmikaLightWhite2 from '../images/lodówkaAmikaLightWhite2.png';
+import lodówkaBekaFreshHarvest2 from '../images/lodówkaBekaFreshHarvest2.png';
+import lodówkaSzamsungMysteriousBlack2 from '../images/lodówkaSzamsungMysteriousBlack2.png';
+import lodówkaAmikaFlashInside2 from '../images/lodówkaAmikaFlashInside2.png';
+import dronGreenUfo2 from '../images/dronGreenUfo2.png';
+import dronCrazyWasp2 from '../images/dronCrazyWasp2.png';
+import kuchniaLelektroluksHotAir2 from '../images/kuchniaLelektroluksHotAir2.png';
+import kuchniaLelektroluksEasyGrill2 from '../images/kuchniaLelektroluksEasyGrill2.png';
+import kuchniaAmikaFastCook2 from '../images/kuchniaAmikaFastCook2.png';
+
+
+export const products = [
+  {
+    'id': 1,
+    'title': 'Pralka Szamsung EXTRA CLEAN',
+    'price': 999.95,
+    'brand': 'Szamsung',
+    'description': 'Urządzenie posiada klasę efektywności energetycznej A+++. Posiada pompę ciepła i pozwala zmniejszyć rachunki za prąd. Pozwala usunąć bakterie, alergeny czy roztocza z pluszowych zabawek, pościeli i ubrań.',
+    'tags': [
+      'AGD',
+      'Do łazienki',
+    ],
+    'image1': pralkaSzajsungEkstraClean,
+    'image2': pralkaSzajsungEkstraClean2,
+  },
+  {
+    'id': 2,
+    'title': 'Pralka Beka MEGA PRO',
+    'price': 889.50,
+    'brand': 'Beka',
+    'description': 'Pralka wolnostojąca z funkcją AquaFusion oraz systemem automatycznej kontroli zużycia wody. Elektroniczny system wyważania wsadu "waży" pranie o odpowiedniu rozkłada je wewnątrz bębna, by wyeliminować wibracje i hałas i zapewnić maksymalną stabilność podczas wirowania. System automatycznej kontroli zużycia wody dobiera jej dokładnie tyle, by w zależności od wagi i rodzaju tkanin zapewnić optymalną skuteczność prania,',
+    'tags': [
+      'AGD',
+      'Do łazienki',
+    ],
+    'image1': pralkaBekaMegaPro,
+    'image2': pralkaBekaMegaPro2,
+  },
+  {
+    'id': 3,
+    'title': 'Odkurzacz Filips SUPER SHINE',
+    'price': 319.99,
+    'brand': 'Filips',
+    'description': 'Elastyczna i zwrotna dolna część drążka, Elektroniczna regulacja siły ssącej na rączce, Ergonomiczny uchwyt, Łatwe manewrowanie odkurzaczem, Łatwe opróżnianie pojemnika na kurz, Oświetlenie LED w dyszy, Przezroczysty pojemnik na kurz, Regulacja mocy, 2w1: odkurzacz pionowy i odkurzacz ręczny.  ',
+    'tags': [
+      'Odkurzacze',
+      'Inne',
+    ],
+    'image1': odkurzaczFilipsSuperShine,
+    'image2': odkurzaczFilipsSuperShine2,
+  },
+  {
+    'id': 4,
+    'title': 'Odkurzacz Szamsung HIPER FLAT',
+    'price': 216.00,
+    'brand': 'Szamsung',
+    'description': 'Bezworkowy, bezprzewodowy, odkurzacz automatyczny, posiadający 7 trybów czyszczenia, wyświetlacz LED, sterowany dotykowo oraz za pomocą WiFi, z systemem Visionary Mapping, FullView Sensor™, w technologii Cyclone Force.Efektywność sprzątania podłoga: 98%; Efektywność sprzątania dywan: 70%; Prędkość: 32 cm/sec; Sterowanie dotykowe; Filtr przedsilnikowy i wylotowy; Szerokość́ szczotki głównej: 311 mm , Sterowanie pilotem z pointerem',
+    'tags': [
+      'Odkurzacze',
+      'Inne',
+    ],
+    'image1': odkurzaczSzamsungHiperFlat,
+    'image2': odkurzaczSzamsungHiperFlat2,
+  },
+  {
+    'id': 5,
+    'title': 'Odkurzacz Lelektroluks DUO POWER',
+    'price': 249.99,
+    'brand': 'Lelektroluks',
+    'description': 'Odkurzacz Lelektroluks DUO POWER może pochwalić się mocą 650W i zasięgiem pracy wynoszącym 7,5 m. Posiada ręczną regulację mocy, którą można dostosować do własnych potrzeb, w celu oszczędzania energii. Dodatkowo wyposażony jest we wskaźnik pełnego worka, miękkie koła, a także metalową rurę teleskopową.',
+    'tags': [
+      'Odkurzacze',
+      'Inne',
+    ],
+    'image1': odkurzaczLelektroluksDuoPower,
+    'image2': odkurzaczLelektroluksDuoPower2,
+  },
+  {
+    'id': 6,
+    'title': 'Telewizor Filips EASY CONTROL',
+    'price': 1999,
+    'brand': 'Filips',
+    'description': 'Elegancki design z imponującym oświetleniem Ambilight. Świetny dźwięk. Sterowanie głosowe AI i klawiatura QWERTY z tyłu pilota ułatwiają wprowadzanie haseł do aplikacji. Sterowanie głosowe oparte na sztucznej inteligencji, wbudowana funkcja Asystent Google i współpraca z usługą Alexa',
+    'tags': [
+      'Telewizory',
+      'Do salonu',
+    ],
+    'image1': telewizorFilipsEasyControl,
+    'image2': telewizorFilipsEasyControl2,
+  },
+  {
+    'id': 7,
+    'title': 'Telewizor Ksajomi GIGA SCREEN',
+    'price': 899.99,
+    'brand': 'Ksajomi',
+    'description': 'Audio Pre-selection Descriptor, Funkcja Adaptacji Dźwięku, Wzmacnianie dialogów. Dopasowany do wieszaków Mini, Montaż ścienny, Możliwość ukrycia przewodów w podstawie, Podstawa',
+    'tags': [
+      'Telewizory',
+      'Do salonu',
+    ],
+    'image1': telewizorKsajomiGigaScreen,
+    'image2': telewizorKsajomiGigaScreen2,
+  },
+  {
+    'id': 8,
+    'title': 'Telewizor Filips ULTRA LIGHT',
+    'price': 985.99,
+    'brand': 'Filips',
+    'description': 'Dopasowanie do ekranu, Powiększenie, Przesunięcie, Rozciągnięcie, Szeroki ekran, Wypełnienie ekranu. Aktualizacja oprogramowania: USB/Internet, Automatyczne wykrywanie urządzeń firmy Filips, EPG.  Łatwa obsługa: sterowanie jednym przyciskiem Home, Sleep Timer, Wstrzymanie odtwarzania programu, Wyłączanie obrazu (dla radia)',
+    'tags': [
+      'Telewizory',
+      'Do salonu',
+    ],
+    'image1': telewizorFilipsUltraLight2,
+    'image2': telewizorFilipsUltraLight,
+  },
+  {
+    'id': 9,
+    'title': 'Głośnik Jotbel PERFECT SOUND',
+    'price': 105.45,
+    'brand': 'Jotbel',
+    'description': 'Przenośny głośnik Jotbel PERFECT SOUND z Bluetooth zapewniający pełne spektrum potężnego dźwięku i posiadający wbudowany bank energii do ładowania urządzeń. Wyposażony jest w opatentowany przetwornik i dwa basowe radiatory Jotbel, które wzmacniają dźwięk silnym, głębokim basem. Wysokowydajny akumulator litowo-jonowy 7500  mAh zapewnia do 20 godzin odtwarzania. Głośnik zawiera wygodne wyjście USB do szybkiego ładowania urządzeń, np. smartfona, aby nigdy nie zostawić użytkownika bez zasilania. ',
+    'tags': [
+      'Sprzęt muzyczny',
+      'Inne',
+    ],
+    'image1': głośnikJotbelPerfectSound,
+    'image2': głośnikJotbelPerfectSound2,
+  },
+  {
+    'id': 10,
+    'title': 'Głośnik Marszal HIGH VOLUME',
+    'price': 99.99,
+    'brand': 'Marszal',
+    'description': 'Wytwarza czyste i precyzyjne brzmienie, nawet przy największej głośności, dzięki zastosowaniu zaawansowanych komponentów, takich jak dwa 15-watowe wzmacniacze klasy D do głośników wysokotonowych i 50-watowy wzmacniacz klasy D dający moc jego subwooferowi. Drewniana konstrukcja obudowy zapewnia ciepłe, naturalne brzmienie, a system bass reflex jest wyposażony w port zwiększający siłę dźwięku przy niskich częstotliwościach.',
+    'tags': [
+      'Sprzęt muzyczny',
+      'Inne',
+    ],
+    'image1': głośnikMarszalHighVolume,
+    'image2': głośnikMarszalHighVolume2,
+  },
+  {
+    'id': 11,
+    'title': 'Lodówka Elgi SIDE BY SIDE',
+    'price': 2999,
+    'brand': 'Elgi',
+    'description': 'Lodówka marki Elgi z technologią Total No Frost o wymiarach 91,2x179x73,8cm. 626 litrów pojemności umożliwia lepszą organizację i mądrzejsze przechowywanie żywności w lodówce. Dzięki Moist Balance Crisper nadmiar wilgoci paruje z żywności i skrapla się na kratce, zamiast gromadzić się wokół jedzenia — w ten sposób w pojemniku utrzymywany jest odpowiedni poziom wilgotności. Poziom maksymalnego hałasu emitowanego przez lodówkę podczas jej pracy wynosi 39 dB. Im większa wartość w decybelach, tym większa głośność w pomieszczeniu.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': lodówkaElgSideBySide,
+    'image2': lodówkaElgSideBySide2,
+  },
+  {
+    'id': 12,
+    'title': 'Lodówka Amika LIGHT WHITE',
+    'price': 1499,
+    'brand': 'Amika',
+    'description': 'Panel sterujący wewnątrz chłodziarki, na którym wyświetlane są cyfry z przypisanym zakresem temperatur, służy do dobrania odpowiednich nastawów w chłodziarce i zamrażarce. Lodówki z funkcją Cichej pracy skonstruowane są tak, by jak najbardziej ograniczyć poziom hałasu generowanego przez mechaniczne elementy. Takie rozwiązanie zapewnia codzienny komfort wszystkim domownikom.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': lodówkaAmikaLightWhite,
+    'image2': lodówkaAmikaLightWhite2,
+  },
+  {
+    'id': 13,
+    'title': 'Lodówka Beka FRESH HARVEST',
+    'price': 1599,
+    'brand': 'Beka',
+    'description': 'Innowacyjna technologia HarvestFresh™ wykorzystująca 3 kolory (niebieski, zielony i czerwony), które naśladują 24-godzinny cykl słoneczny. Dzięki temu witaminy A i C utrzymują się dłużej w owocach i warzywach przechowywanych w lodówce. Dzięki zastosowaniu najnowszej technologii produkty z klasą efektywności energetycznej A++ to gwarancja bardzo niskiego zużycia energii, a produkty w tej klasie nie tylko pomogą podreperować budżet rodzinny, ale również dbają o środowisko naturalne.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': lodówkaBekaFreshHarvest,
+    'image2': lodówkaBekaFreshHarvest2,
+  },
+  {
+    'id': 14,
+    'title': 'Lodówka Szamsung MYSTERIOUS BLACK',
+    'price': 3319,
+    'brand': 'Szamsung',
+    'description': 'Interaktywny wyświetlacz umieszczony na drzwiach lodówki na bieżąco informuje o aktywnych funkcjach urządzenia. Dzięki niemu nie musisz otwierać chłodziarki, żeby zmienić jej ustawienia. Technologia Space Max umożliwiła pogłębienie i poszerzenie przestrzeni użytkowej chłodziarko-zamrażarki. Wyjątkowy i wysoce wydajny system izolacji sprawił, że ścianki urządzenia stały się cieńsze i wewnątrz do dyspozycji mamy większą pojemność. W tak zaprojektowanej lodówce zmieścisz więcej, mimo że parametry zewnętrzne się nie zmieniły.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': lodówkaSzamsungMysteriousBlack,
+    'image2': lodówkaSzamsungMysteriousBlack2,
+  },
+  {
+    'id': 15,
+    'title': 'Lodówka Amika FLASH INSIDE',
+    'price': 1997,
+    'brand': 'Amika',
+    'description': 'Zestaw balkoników idealnych do bezpiecznego przechowywania małych produktów, słoików czy butelek. Teraz zawsze będziesz wiedzieć, gdzie co jest – i będziesz mieć do tego wygodny dostęp. Wykorzystując technologię LED, dbasz o środowisko i stan swojego portfela, bo zużywasz mniej energii.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': lodówkaAmikaFlashInside,
+    'image2': lodówkaAmikaFlashInside2,
+  },
+  {
+    'id': 16,
+    'title': 'Dron GREEN UFO',
+    'price': 209.95,
+    'brand': 'Nasa',
+    'description': 'Model GREEN UFO to bardzo funkcjonalny i niezwykle łatwy w obsłudze dron, który gwarantuje świetną zabawę każdemu wielbicielowi podniebnych akrobacji. Automatyczne utrzymywanie wysokości lotu,  możliwość zawiśnięcia w powietrzu, wbudowana kamera i wytrzymała konstrukcja, to główne cechy tego urządzenia. Dzięki możliwości zawiśnięcia w powietrzu, możemy uzyskać niepowtarzalne ujęcia oraz jeszcze dokładniej sterować dronem.',
+    'tags': [
+      'Wideorejestratory',
+      'Inne',
+    ],
+    'image1': dronGreenUfo,
+    'image2': dronGreenUfo2,
+  },
+  {
+    'id': 17,
+    'title': 'Dron CRAZY WASP',
+    'price': 399.99,
+    'brand': 'Ryaner',
+    'description': 'Mały dron idealny dla początkujących z łatwą obsługą. Sterowanie za pomocą aplikacji. Lekka, a zarazem wytrzymała konstrukcja Tello skutecznie ochraniająca podzespoły sprawi, że będziesz latać z dużą pewnością i swobodą.',
+    'tags': [
+      'Wideorejestratory',
+      'Inne',
+    ],
+    'image1': dronCrazyWasp,
+    'image2': dronCrazyWasp2,
+  },
+  {
+    'id': 18,
+    'title': 'Kuchnia Lelektroluks HOT AIR',
+    'price': 2199,
+    'brand': 'Lelektroluks',
+    'description': 'Kuchnia elektryczna Lelektroluks HOT AIR z płytą indukcyjną o szerokości 50 cm, głębokości 60cm, oraz piekarnikiem o pojemności komory 58 l, z technologią Hot Air, funkcją SteamBake, z systemem pieczenia konwekcyjnego i funkcją pasteryzacji, suszenia owoców, w klasie energetycznej A. Zakres temperatur: 50°C - 250°C; Powierzchnia największej płyty grzewczej - górny piekarnik [cm2] : 1164; Moc grzałki termoobiegu [W]: 2000; Zakres temperatur: 50°C - 250°C;',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': kuchniaLelektroluksHotAir,
+    'image2': kuchniaLelektroluksHotAir2,
+  },
+  {
+    'id': 19,
+    'title': 'Kuchnia Lelektroluks EASY GRILL',
+    'price': 1119,
+    'brand': 'Lelektroluks',
+    'description': 'Kuchnia elektryczna Lelektroluks EASY GRILL z płytą indukcyjną, wielofunkcyjna, o pojemności 72 l,  o szerokości 59.6 cm, z 4 polami grzejnymi Hilight, z termoobiegiem, grillem, wyjmowaną szybą wewnętrzną, w klasie energetycznej A. Szuflada na naczynia, Tylny wylot oparów z piekarnika, Wentylator.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': kuchniaLelektroluksEasyGrill,
+    'image2': kuchniaLelektroluksEasyGrill2,
+  },
+  {
+    'id': 20,
+    'title': 'Kuchnia Amika FAST COOK',
+    'price': 1097,
+    'brand': 'Amika',
+    'description': 'Szuflada na prowadnicach rolkowych. Mechaniczne ustawienie temperatury, Przyciski z wyświetlaczem, 2 + 4 pokrętła mocy. Programator elektroniczny. 10 funkcji piekarnika, to jeszcze więcej możliwości pieczenia - od rozmrażania do grillowania.',
+    'tags': [
+      'AGD',
+      'Do kuchni',
+    ],
+    'image1': kuchniaAmikaFastCook,
+    'image2': kuchniaAmikaFastCook2,
+  },
+];
