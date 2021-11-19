3c92b435569e1f96bb31ced1d10836d8d413011a
Michał Lisowiec <miclisowiec@gmail.com>
2021-11-19 20:45:44 +0100

    Persist redux state on refresh

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
 import { createGlobalStyle } from 'styled-components';
 
 
@@ -22,8 +23,10 @@ const GlobalStyle = createGlobalStyle`
 
 ReactDOM.render(
   <Provider store={store}>
-    <GlobalStyle/>
-    <App />
+    <PersistGate persistor={persistor}>
+      <GlobalStyle/>
+      <App />
+    </PersistGate>
   </Provider>, 
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import {products} from '../products/products';
 import {combineReducers, createStore} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import filtersReducer from './filtersRedux';
 import cartReducer from './cartRedux';
 
 
+// create initial state for filters
 export const filtersInitialState = {
   searchPhrase: '',
   checkedTags: [],
@@ -14,43 +17,53 @@ export const filtersInitialState = {
   },
   sortingKey: '---',
 };
-
+  
+// create initial state for cart
 export const cartInitialState = {
   productsInCart: [],
   quantityInCart: 0,
   totalPrice: 0,
   delivery: 20,
 };
-
-
+  
+// define initial state
 const initialState = {
   products,
   filters: filtersInitialState,
   cart: cartInitialState,
 };
-
-
+  
 // define reducers
 const reducers = {
   filters: filtersReducer,
   cart: cartReducer,
 };
-  
+    
 // add blank reducers for initial state properties without reducers
 Object.keys(initialState).forEach(item => {
   if (typeof reducers[item] == 'undefined') {
     reducers[item] = (statePart = null) => statePart;
   }
 });
-  
+
+//define persistConfig  
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+    
 // combine reducers
 const combinedReducers = combineReducers(reducers);
-
-
-const store = createStore(
-  combinedReducers,
+  
+// define persisted reducers
+const persistedReducers = persistReducer(persistConfig, combinedReducers);
+  
+// create store
+export const store = createStore(
+  persistedReducers,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-    
-export default store;
+  
+// define persistor
+export const persistor = persistStore(store);
